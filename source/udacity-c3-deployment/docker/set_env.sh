b4c4c7365e93cb7919b6736518412ee263b57f89
arbidha <arbidha1412@gmail.com>
2020-05-02 12:42:08 -0500

    Docker build complet

---
+++ source/udacity-c3-deployment/docker/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export PATH=$PATH:/usr/local/mysql/bin/
+export POSTGRES_USERNAME=udagramdev 
+export POSTGRES_PASSWORD=udagramdev
+export POSTGRES_HOST=udagramdev.cvpxacccfvki.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=udagramdev
+export AWS_BUCKET=udagramdev-4152020
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=helloworld
+export URL=http://localhost:8100
